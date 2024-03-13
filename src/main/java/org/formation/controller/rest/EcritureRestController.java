42632afc4bf5eb84eed08cd0c4b96fc8ab60b5fa
bayefallkamal <moustaphakamalkane@gmail.com>
2024-03-13 23:13:01 +0000

    pageable table list ecritures

--- src/main/java/org/formation/controller/rest/EcritureRestController.java
+++ src/main/java/org/formation/controller/rest/EcritureRestController.java
@@ -12,10 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/ecritures")
